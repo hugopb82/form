5d47c2dbf6f43b578dbc2e009332c11e41bdfee0
hugopb82 <hugopb@neuf.fr>
2015-02-17 11:34:08 +0100

    Optimisation!!!!!

--- src/form.php
+++ src/form.php
@@ -1,288 +1,88 @@
 <?php
-	
+/*
+*	Form class by hugopb82, enjoy!
+*	Fork me on github!
+*/
+class Form{
+
+	private $data = [];
+	private $end;
+
 	/*
-	*	Form class by hugopb82
-	*	Follow me on twitter @hugopb82
-	*	Fork me on github
+	* Set form attributes
+	*	@param	$data	array	form attributes
 	*/
-	
-	// Main class
-	class form{
-		private $FORM_ATTRIBUTES = array('accept-charset', 'action', 'autocomplete', 'enctype', 'method', 'name', 'novalidate', 'target', 'id', 'class');
-		
-		public $html = "";
-		public $params = array();
-		
-		/**
-		* Set form attributes
-		* @params	text/array	$data	list of form parameters
-		*/
-		public function __construct($data = null){
-			if($data == null){
-				return false;
-			}elseif(is_array($data)){
-				foreach($data as $k => $v){
-					if(in_array(strtolower($k), $this->FORM_ATTRIBUTES)){
-						$this->params[strtolower($k)] = $v;
-					}
-				}
-			}else{
-				$this->params['id'] = $data;
-			}
-		}
-		
-		/**
-		* Add code to form
-		* @params	text	$object		html code the form object return
-		*/
-		public function add($object){
-			$this->html .= $object;
-		}
-		
-		/**
-		* Add code to form
-		* @return	text/html	the html form code
-		*/
-		public function build(){
-			$html = '<form ';
-			foreach($this->params as $k => $v){
-				if(in_array($k, $this->FORM_ATTRIBUTES)){
-					$html .= $k.'="'.$v.'" ';
-				}
-			}
-			$html .= '>'.$this->html.'</form>';
-			return $html;
-		}
+	public function __construct($data){
+		$this->data = is_array($data) ? $data : array('action' => $data);
 	}
-	
-	
-	
-	// Html forms elements
-	
-	// Main form elements function
-	class Elements{
-		
-		public $html;
-		
-		/**
-		* Return the code when object called
-		* @return	text/html	the html element code
-		*/
-		function __toString() {
-			return $this->html;
+
+	/*
+	* Add an input in the form
+	*	@param	$type	string	type of the form field
+	*	@param	$name	string	name of the form field
+	*	@param	$label	string	label of the form field (default is $name)
+	*	@param	$options	array	attributes for the fields
+	*	@return	string	final html code
+	*/
+	public function add($type, $name, $label = null, $options = null){
+		$label = !is_null($label) ? $label : ucfirst($name) . ' :';
+		$attributes = '';
+
+		if(!is_null($options) && is_array($options) && $type != 'select'){
+			foreach($options as $k => $v){
+				$attributes .= $k . '="' . $v . '" ';
+			}
 		}
-	
+
+		if($type === 'submit'){
+			$end = '<input type="submit" name="' . $name . '" value="' . $label . '"  ' . $attributes .' >';
+		}elseif($type === 'textarea'){
+			$input = '<textarea name="' . $name . '"  ' . $attributes .' ></textarea>';
+			$end = $this->label($label, $input);
+		}elseif($type === 'hidden'){
+			$end = '<input type="hidden" name="' . $name . '">';
+		}elseif($type === 'select'){
+			$html = '<select name="' . $name . '">';
+			foreach($options as $k => $v){
+				if(is_array($options[$k])){
+					$attributes = '';
+					foreach($v as $k => $v){
+						$attributes .= $k . '="' . $v . '" ';
+					}
+					$html .= '<option ' . $attributes . ' >' . $k . '</option>';
+				}else{
+					$html .= '<option>' . $v . '</option>';
+				}
+			}
+			$html .= '</select>';
+			$end = $this->label($label, $html);;
+		}else{
+			$input = '<input type="' . $type . '" name="' . $name . '" ' . $attributes .' >';
+			$end = $this->label($label, $input);
+		}
+		$this->end .= $end;
 	}
-	
-		// Add html to form
-		class html extends Elements{
-			
-			public function __construct($code){
-				$this->html = $code;
-			}
-			
+
+	/*
+	* Build the final html code
+	*	@return	string	final html code
+	*/
+	public function end(){
+		$html = '<form ';
+		foreach($this->data as $k => $v){
+			$html .= $k . '="' . $v . '" ';
 		}
-		
-		// Add submit button to form
-		class submit extends Elements{
-			
-			public function __construct($label, $name, $options = null){
-				$atrr = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				$html = '<input value="'.$label.'" type="submit" name="'.$name.'" id="'.$name.'" '.$atrr.'>';
-				$this->html = $html;
-			}
-			
-		}
-		
-		// Add reset button to form
-		class reset extends Elements{
-			
-			public function __construct($label, $name, $options = null){
-				$atrr = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				$html = '<input value="'.$label.'" type="reset" name="'.$name.'" id="'.$name.'" '.$atrr.'>';
-				$this->html = $html;
-			}
-			
-		}
-		
-		// Add button to form
-		class button extends Elements{
-			
-			public function __construct($label, $name, $options = null){
-				$atrr = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				$html = '<input value="'.$label.'" type="button" name="'.$name.'" id="'.$name.'" '.$atrr.'>';
-				$this->html = $html;
-			}
-			
-		}
-		
-		// Add text field to form
-		class text extends Elements{
-			
-			public function __construct($label, $name, $options = null){
-				$atrr = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				$html = '<label for="'.$name.'">'.$label.'</label><input type="text" name="'.$name.'" id="'.$name.'" '.$atrr.'>';
-				$this->html = $html;
-			}
-			
-		}
-		
-		// Add password field to form
-		class password extends Elements{
-			
-			public function __construct($label, $name, $options = null){
-				$atrr = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				$html = '<label for="'.$name.'">'.$label.'</label><input type="password" name="'.$name.'" id="'.$name.'" '.$atrr.'>';
-				$this->html = $html;
-			}
-			
-		}
-		
-		// Add file field to form
-		class file extends Elements{
-			
-			public function __construct($label, $name, $options = null){
-				$atrr = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				$html = '<label for="'.$name.'">'.$label.'</label><input type="file" name="'.$name.'" id="'.$name.'" '.$atrr.'>';
-				$this->html = $html;
-			}
-			
-		}
-		
-		// Add textarea field to form
-		class textarea extends Elements{
-			
-			public function __construct($label, $name, $options = null){
-				$atrr = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				$html = '<label for="'.$name.'">'.$label.'</label><textarea name="'.$name.'" id="'.$name.'" '.$atrr.'></textarea>';
-				$this->html = $html;
-			}
-			
-		}
-		
-		// Add select field to form
-		class select extends Elements{
-			
-			public function __construct($label, $name, $list, $options = null){
-				$atrr = "";
-				$list_options = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				foreach($list as $v){
-					$attr2 = "";
-					if(isset($v[1]) && is_array($v[1])){
-						foreach($v[1] as $w => $x){
-							$attr2 .= $w.'="'.$x.'" ';
-						}
-					}
-					$list_options .= '<option value="'.$v[0].'" '.$attr2.'>'.$v[0].'</option>';
-				}
-				$html = '<label for="'.$name.'">'.$label.'</label><select name="'.$name.'" id="'.$name.'" '.$atrr.'>'.$list_options.'</select>';
-				$this->html = $html;
-			}
-			
-		}
-		
-		// Add radio field to form
-		class radio extends Elements{
-			
-			public function __construct($label, $name, $list, $options = null){
-				$atrr = "";
-				$html = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				foreach($list as $v){
-					$attr2 = "";
-					if(isset($v[1]) && is_array($v[1])){
-						foreach($v[1] as $w => $x){
-							$attr2 .= $w.'="'.$x.'" ';
-						}
-					}
-					$html .= '<label for="'.$name.$v[0].'">'.$v[0].'</label><input type="radio" value="'.$v[0].'" name="'.$name.'" id="'.$name.$v[0].'" '.$attr2.'>';
-				}
-				$this->html = $html;
-			}
-			
-		}
-		
-		// Add checkbox field to form
-		class checkbox extends Elements{
-			
-			public function __construct($label, $name, $list, $options = null){
-				$atrr = "";
-				$html = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				foreach($list as $v){
-					$attr2 = "";
-					if(isset($v[1]) && is_array($v[1])){
-						foreach($v[1] as $w => $x){
-							$attr2 .= $w.'="'.$x.'" ';
-						}
-					}
-					$html .= '<label for="'.$name.$v[0].'">'.$v[0].'</label><input type="checkbox" value="'.$v[0].'" name="'.$name.'" id="'.$name.$v[0].'" '.$attr2.'>';
-				}
-				$this->html = $html;
-			}
-			
-		}
-		
-		// Add color field to form
-		class color extends Elements{
-			
-			public function __construct($label, $name, $options = null){
-				$atrr = "";
-				if($options != null && is_array($options)){
-					foreach($options as $k => $v){
-						$atrr .= $k.'="'.$v.'" ';
-					}
-				}
-				$html = '<label for="'.$name.'">'.$label.'</label><input type="color" name="'.$name.'" id="'.$name.'" '.$atrr.'>';
-				$this->html = $html;
-			}
-			
-		}
+		$html .= '>' . $this->end . '</form>';
+		return $html;
+	}
+
+	/*
+	* Warp the input with a label
+	*	@param	$label	string	value of the label
+	*	@param	$input	string	html input code
+	*	@return	string	final html code
+	*/
+	protected function label($label, $input){
+		return '<label>' . $label . $input . '</label>';
+	}
+}
