ca99e7c7663d1f287215c2995b508da24aca3104
hugopb82 <hugopb@neuf.fr>
2015-02-17 11:38:16 +0100

    Simplification

--- test/test.php
+++ test/test.php
@@ -1,23 +1,32 @@
 <?php
-	require('form.php');
-	
-	$form = new form(array('id' => 'myform', 'action' => ''));
-	
-	$form->add(new text('Pseudo : ', 'pseudo', array('disabled' => '1')));
-	$form->add(new password('Pass : ', 'pass'));
-	$form->add(new file('File : ', 'file'));
-	$form->add(new file('Textarea : ', 'textarea'));
-	$form->add(new select('Country : ', 'country', array(array('France'), array('France'))));
-	$form->add(new radio('Gender : ', 'gender', array(array('man', array('checked' => '1')), array('woman'))));
-	$form->add(new checkbox('Gender : ', 'gender', array(array('man', array('checked' => '1')), array('woman'))));
-	$form->add(new color('Color : ', 'color'));
-	$form->add(new button('Button', 'button'));
-	$form->add(new submit('Send', 'submit'));
-	$form->add(new reset('Reset', 'reset'));
-	
-	$html = $form->build();
-	
-	echo htmlspecialchars($html);
-	echo "<hr />";
-	echo $html;
+require '../src/form.php';
+
+// form attributes
+$data = array(
+	'id' => 'form',
+	'action' => 'zoz',
+	'method' => 'post'
+	);
+
+
+$form = new Form($data);
+
+// Syntax : $form->add(type, name, label, options)
+// Exception for select : $form->add('select', name, label, array(item' => array(options), item, item))
+$form->add('text', 'title', null, array('disabled' => 'disabled'));
+$form->add('password', 'password');
+$form->add('textarea', 'content', 'Content :', array('id' => 'efef'));
+$form->add('radio', 'gender',	'Male :', array('value' => 'male'));
+$form->add('radio', 'gender',	'Female :', array('value' => 'female'));
+$form->add('color', 'color');
+$form->add('date', 'date');
+$form->add('number', 'number', null, array('min' => '4', 'max' => '18') );
+$form->add('search', 'search');
+$form->add('url', 'url');
+$form->add('email', 'email');
+$form->add('file', 'file');
+$form->add('select', 'model', null, array('one', 'two', 'three'));
+$form->add('hidden', 'id');
+$form->add('submit', 'submit', 'Save :');
+echo($form->end());
 ?>
